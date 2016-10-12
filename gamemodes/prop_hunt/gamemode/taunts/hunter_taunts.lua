f8a2145494fc26ba81ab0c59ab7d83232c79cd42
Vinzuerio <wolvindra.vinzuerio@gmail.com>
2016-10-12 11:05:19 -0400

    First Public Commit 1.4

--- gamemodes/prop_hunt/gamemode/taunts/hunter_taunts.lua
+++
@@ -1,8 +0,0 @@
--- Help & documentation of how to add your custom taunts: http://www.wolvindra.net/phe_faq
-
--- HUNTER TAUNT LISTS
-PH_TAUNT_CUSTOM.HUNTER = {
-	-- Same as prop_taunts.lua, except this only for Team Hunter.
-	"vo/k_lab/ba_guh.wav",
-	"taunts/props_extra/dx_augmented.wav"
-}
