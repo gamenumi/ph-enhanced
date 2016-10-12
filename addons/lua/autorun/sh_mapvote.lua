0dfd36b81725f3a89793724ab0d2e444fbbe627a
Vinzuerio <Vinzuerio@users.noreply.github.com>
2016-10-12 16:03:19 +0700

    Update sh_mapvote.lua

--- addons/lua/autorun/sh_mapvote.lua
+++ addons/lua/autorun/sh_mapvote.lua
@@ -19,10 +19,10 @@ MapVoteConfigDefault = {
 local convarlist = {
 	{var = "mv_maplimit", 		val = "24", 	flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE }, help = "numbers of map that shown on mapvote." },
 	{var = "mv_timelimit",		val = "28", 	flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "time in second for default mapvotes time." },
-	{var = "mv_allowcurmap",	val = "0", 		flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE }, help = "allow current map to be voted (1/0)" },
-	{var = "mv_cooldown",		val = "1", 		flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "enable cooldown for voting a map" },
-	{var = "mv_mapbeforerevote",val = "2", 		flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "how many times that the map which cooldown can be shown again?" },
-	{var = "mv_rtvcount",		val = "3", 		flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "number of required players to use rtv mapvote." },
+	{var = "mv_allowcurmap",	val = "0", 	flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE }, help = "allow current map to be voted (1/0)" },
+	{var = "mv_cooldown",		val = "1", 	flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "enable cooldown for voting a map" },
+	{var = "mv_mapbeforerevote",	val = "2", 	flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "how many times that the map which cooldown can be shown again?" },
+	{var = "mv_rtvcount",		val = "3", 	flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY }, help = "number of required players to use rtv mapvote." },
 	{var = "mv_mapprefix",		val = "ph_,cs_,de_", flags = {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE }, help = "Map Prefixes that will be shown under mapvote. Use the following example:\n  \"ph_,cs_,de_\" (Dont forget to use quotation marks!)." }
 }
 
