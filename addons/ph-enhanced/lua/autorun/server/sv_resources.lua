e52524b3ce15f113891e7a0268d09d1926aafcf1
Vinzuerio <wolvindra.vinzuerio@gmail.com>
2018-05-13 01:27:09 +0800

    Removed unnecessary leftover files

--- addons/ph-enhanced/lua/autorun/server/sv_resources.lua
+++
@@ -1,7 +0,0 @@
--- Serverside additionals
--- when a player is really lazy and had to use manual/FastDL download:
-resource.AddSingleFile("materials/vgui/hud/wlv_bren.vmt")
-resource.AddSingleFile("materials/vgui/hud/wlv_bren.vtf")
-
--- Ship
-resource.AddWorkshop("417565863")
